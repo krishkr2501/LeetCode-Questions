cd95193b787c4342c1dca8e19647829bbe067973
KRISH KUMAR <sahaykrish1526@outlook.com>
2026-03-14 19:23:27 +0530

    Time: 10 ms (100%), Space: 154.2 MB (93.75%) - LeetHub

--- 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
@@ -1,33 +1,30 @@
 class Solution {
     public long[] getDistances(int[] arr) {
-        long[] output = new long[arr.length];
-        Map<Integer, Long> sumMap = new HashMap<>();
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int i = 0; i < arr.length; ++ i) {
-            if (!sumMap.containsKey(arr[i])) {
-                sumMap.put(arr[i], 0l);
-                countMap.put(arr[i], 0);
-            }
-            
-            output[i] += i * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
-            sumMap.put(arr[i], sumMap.get(arr[i]) + i);
-            countMap.put(arr[i], countMap.get(arr[i]) + 1);
+        int n = arr.length, m1 = arr[0], m2 = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < m1)
+                m1 = arr[i];
+            if (arr[i] > m2)
+                m2 = arr[i];
         }
-        
-        sumMap = new HashMap<>();
-        countMap = new HashMap<>();
-        int len = arr.length;
-        for (int i = len - 1; i >= 0; -- i) {
-            if (!sumMap.containsKey(arr[i])) {
-                sumMap.put(arr[i], 0l);
-                countMap.put(arr[i], 0);
-            }
-            
-            output[i] += (len - i - 1) * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
-            sumMap.put(arr[i], sumMap.get(arr[i]) + (len - i - 1));
-            countMap.put(arr[i], countMap.get(arr[i]) + 1);
+        m2 = m2 - m1 + 1;
+        long[] s1 = new long[m2];
+        long[] s2 = new long[m2];
+        int[] c1 = new int[m2];
+        int[] c2 = new int[m2];
+        for (int i = 0; i < n; i++) {
+            s2[arr[i] - m1] += i;
+            c2[arr[i] - m1] += 1;
         }
-        
-        return output;
+        long[] r = new long[n];
+        for (int i = 0; i < n; i++) {
+            int x = arr[i] - m1;
+            r[i] = (1L * c1[x] * i - s1[x]) + (s2[x] - 1L * i * c2[x]);
+            s1[x] += i;
+            s2[x] -= i;
+            c1[x] += 1;
+            c2[x] -= 1;
+        }
+        return r;
     }
 }
