7cbde3dc8f0c684bafe0e723e718751c9bc5ddcf
KRISH KUMAR <sahaykrish1526@outlook.com>
2026-03-17 10:42:43 +0530

    Time: 12 ms (100%), Space: 113.6 MB (80.87%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -2,30 +2,22 @@ class Solution {
     public int largestSubmatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int ans = 0;
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 1) {
-                    matrix[i][j] += matrix[i - 1][j];
-                }
+        for(int i =1; i< m; i++) {
+            for(int j=0; j<n; j++) {
+                if(matrix[i][j]==0) continue;
+                matrix[i][j] += matrix[i-1][j];
             }
         }
-
-        for (int i = 0; i < m; i++) {
-            Integer[] row = new Integer[n];
-            for (int j = 0; j < n; j++) {
-                row[j] = matrix[i][j];
-            }
-
-            Arrays.sort(row, Collections.reverseOrder());
-
-            for (int width = 1; width <= n; width++) {
-                int h = row[width - 1];
-                ans = Math.max(ans, h * width);
+        int maxArea = 0;
+        for(int i=0; i<m; i++) {
+            Arrays.sort(matrix[i]);
+            for(int j=0; j<n; j++) {
+                int width = n-j;
+                maxArea = Math.max(maxArea, width * matrix[i][j]);
+                
             }
         }
-
-        return ans;
+        return maxArea;
     }
 }
