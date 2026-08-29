7fdb927596a5ef5c12c70012881943c9d438a3c4
KRISH KUMAR <sahaykrish1526@outlook.com>
2026-08-29 22:30:37 +0530

    Time: 1 ms (15.38%), Space: 42.6 MB (86.11%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashSet<Character> hs = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(hs.contains(s.charAt(i))){
+                return s.charAt(i);
+            }
+            hs.add(s.charAt(i));
+        }
+        return 'a';
+    }
+}
